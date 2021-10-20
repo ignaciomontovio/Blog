e9a95d790ef88fd373324f546212e0d0ba12786a
ignaciomontovio <ignaciomontovio@gmail.com>
2021-10-20 01:04:05 -0300

    First Commit

---
+++ src/test/java/com/g98/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.g98.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
